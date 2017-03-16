115eef53cff9191ada3590bfa07d10c50bff4f80
Archer70 <code.archer70@gmail.com>
2017-03-16 16:31:03 -0700

    Change example text to Bacon ipsum.
    Dedicated to NanoSector and Antes.

--- cs_template/Item.template.php
+++ cs_template/Item.template.php
@@ -8,7 +8,7 @@ function template_main()
         <img id="item_thumbnail" src="https://avatars0.githubusercontent.com/u/2660927?v=3&s=460" alt="">
         <div id="item_description">
             <div id="item_photo_slider">Image slider</div>
-            <p>Description text, paragraph one.</p>
-            <p>Description text, paragraph two.</p>
+            <p>Bacon ipsum dolor amet ribeye meatloaf pork belly porchetta. Capicola frankfurter shoulder pancetta andouille picanha spare ribs chicken salami kevin short ribs. Cow tongue ribeye ham pork belly porchetta pastrami venison. Turducken jerky tail, prosciutto burgdoggen meatball hamburger kevin sirloin ribeye meatloaf cow doner. Pork belly tail turducken bacon, shankle sirloin pastrami fatback ribeye boudin shank picanha alcatra salami. Sausage drumstick sirloin pork belly.</p>
+            <p>Turkey short loin boudin ball tip cow frankfurter drumstick alcatra pig. Tenderloin pork tongue spare ribs meatball, jowl meatloaf beef ribs shoulder ball tip turducken t-bone boudin. Pastrami flank t-bone, bacon alcatra frankfurter bresaola jerky beef ribs. Chuck shankle shank, picanha rump swine ground round. Leberkas doner bresaola sausage boudin pig short ribs, andouille cow ribeye prosciutto tenderloin turkey ham hock.</p>
         </div>';    
 }
