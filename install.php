957e0c7229edc2d71408a50b80751da5a008ec0c
Archer70 <code.archer70@gmail.com>
2017-03-16 17:49:08 -0700

    Change integration hook function.
    add_integration_function(), not add_integration_hook(). I should read more carefully.

--- install.php
+++ install.php
@@ -1,4 +1,4 @@
 <?php
 
-add_integration_hook('integrate_pre_include', '$sourcedir/cs_source/hook_function/IntegrateAction.php', true);
-add_integration_hook('integrate_actions', 'csIntegrateAction', true);
+add_integration_function('integrate_pre_include', '$sourcedir/cs_source/hook_function/IntegrateAction.php', true);
+add_integration_function('integrate_actions', 'csIntegrateAction', true);
