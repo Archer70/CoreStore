dc8d34d9c586fe3c38291a3e8261c0052bf20178
Archer70 <code.archer70@gmail.com>
2017-03-28 21:00:17 -0700

    Create mustache engine factory.

--- cs_template/Item.template.php
+++ cs_template/Item.template.php
@@ -2,31 +2,26 @@
 
 function template_main()
 {
-    global $scripturl, $context, $txt, $settings, $sourcedir;
+    global $context, $txt, $settings;
 		
-	// I can't think of a good reason making text bbc presentable shouldn't be a template operation.
-	$context['cs_item']['description'] = parse_bbc($context['cs_item']['description']);
-	$comments = $context['cs_item_comments'];
-	foreach ($comments as $id => $comment) {
-		$comments[$id]['body'] = parse_bbc($comment['body']);
-	}
-	
-	require_once($sourcedir . '/cs_source/lib/mustache.php');
-	$mustache = new Mustache_Engine([
-		'escape' => function($value) {
-			return $value;
-			
-		},
-		'cache' => $settings['theme_dir'] . '/cs_template/mustache/cache',
-		'loader' => new Mustache_Loader_FilesystemLoader($settings['theme_dir'] . '/cs_template/mustache'),
-		'partials_loader' => new Mustache_Loader_FilesystemLoader($settings['theme_dir'] . '/cs_template/mustache/partials'),
-	]);
+	formatComments();	
 
-	$tpl = $mustache->loadTemplate('item');
+	$tpl = $context['mustache']->loadTemplate('item');
 	echo $tpl->render([
 		'settings' => $settings,
 		'context' => $context,
 		'txt' => $txt,
 		'comments' => $comments,
 	]);
+}
+
+function formatComments()
+{
+	global $context;
+	// I can't think of a good reason making text bbc presentable shouldn't be a template operation.
+	$context['cs_item']['description'] = parse_bbc($context['cs_item']['description']);
+	$comments = $context['cs_item_comments'];
+	foreach ($comments as $id => $comment) {
+		$comments[$id]['body'] = parse_bbc($comment['body']);
+	}
 }
