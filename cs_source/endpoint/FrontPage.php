1aa0c4f337d08b333bd5fcb148fb6d6d6ad63c9b
Archer <code.archer70@gmail.com>
2017-04-12 19:44:17 -0700

    Add category links to front page.

--- cs_source/endpoint/FrontPage.php
+++ cs_source/endpoint/FrontPage.php
@@ -1,5 +1,7 @@
 <?php
 use CoreStore\interactors\FrontPageInteractor;
+use CoreStore\interactors\CategoryInteractor;
+use CoreStore\data_managers\smf_mysql\CategoryStorage;
 use CoreStore\data_managers\smf_mysql\FrontPageStorage;
 use CoreStore\utilities\MustacheFactory;
 
@@ -8,16 +10,19 @@ require __DIR__ . '/../TestBootstrap.php';
 function csFrontPage()
 {
 	global $context, $txt;
-	
+
 	loadLanguage('cs_language/CoreStore');
-	
+
 	$interactor = new FrontPageInteractor(new FrontPageStorage());
 	$interactor->loadItems();
-	
+
+	$categories = new CategoryInteractor(
+		new CategoryStorage());
+	$context['categories'] = $categories->getAllCategories();
+
 	$context['page_title'] = $txt['core_store'];
-	
+
 	$context['mustache'] = MustacheFactory::getMustacheEngine();
-	
+
 	loadTemplate('cs_template/FrontPage', 'cs_styles/front_page');
 }
-
